ad2bbaa473a97b7b465445dc387f2b28945905d2
Koks Nieger <koksnieger@gmail.com>
2012-06-19 17:29:10 +0100

    Initial script skeleton.

---
+++ src/org/halfdungeon/HalfDungeon.java
@@ -0,0 +1,20 @@
+package org.halfdungeon;
+
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.Manifest;
+
+@Manifest(
+        name = "HalfDungeon",
+        version = 0.1,
+        description = "RuneScape F2P Dungeoneering Bot.",
+        authors = {"koksnieger"},
+        website = "http://github.com/koksnieger/HalfDungeon"
+)
+public final class HalfDungeon extends ActiveScript
+{
+    @Override
+    protected void setup()
+    {
+        // TODO: fill this skeleton
+    }
+}
