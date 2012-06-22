9557fa69030955311cff6fdb8ca59f1a24cc43a6
Koks Nieger <koksnieger@gmail.com>
2012-06-22 17:51:14 +0100

    Added a helper function for a better random sleep.

--- src/org/half/utils/Misc.java
+++ src/org/half/utils/Misc.java
@@ -2,6 +2,7 @@ package org.half.utils;
 
 import org.powerbot.game.api.methods.Game;
 import org.powerbot.game.api.methods.Walking;
+import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.wrappers.Tile;
 
 public class Misc
@@ -15,5 +16,16 @@ public class Misc
         int xOff = x - Game.getBaseX() - Walking.getCollisionOffset(gamePlane).getX();
         int yOff = y - Game.getBaseY() - Walking.getCollisionOffset(gamePlane).getY();
         return flags[xOff][yOff];
+    }
+
+    /**
+     * Sleeps current thread for a minimum time up to an exponential random value.
+     * (this makes things harder to detect than the most common linear random most people use)
+     *
+     * @param minimumTime The minimum time to sleep.
+     */
+    public static void randomSleep(int minimumTime)
+    {
+        Time.sleep(minimumTime - (int) (minimumTime * 0.5 * Math.log(Math.random())));
     }
 }
