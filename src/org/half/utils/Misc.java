b8b496ea66220a71dda7fd6be6e293fc7b1952ef
Koks Nieger <koksnieger@gmail.com>
2012-06-25 16:16:59 +0100

    Renamed randomSleep to sleepExponential and added sleepUntil.

--- src/org/half/utils/Misc.java
+++ src/org/half/utils/Misc.java
@@ -1,5 +1,6 @@
 package org.half.utils;
 
+import org.powerbot.concurrent.strategy.Condition;
 import org.powerbot.game.api.methods.Game;
 import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.util.Time;
@@ -23,9 +24,29 @@ public class Misc
      * (this makes things harder to detect than the most common linear random most people use)
      *
      * @param minimumTime The minimum time to sleep.
+     * @return The number of actual slept milliseconds.
      */
-    public static void randomSleep(int minimumTime)
+    public static int sleepExponential(int minimumTime)
     {
-        Time.sleep(minimumTime - (int) (minimumTime * 0.5 * Math.log(Math.random())));
+        int time = minimumTime - (int) (minimumTime * 0.5 * Math.log(Math.random()));
+        Time.sleep(time);
+        return time;
+    }
+
+    /**
+     * Sleeps current thread in random intervals until a condition is met.
+     *
+     * @param condition Condition that must be met for the sleep to end.
+     * @return The number of milliseconds slept.
+     */
+    public static int sleepUntil(final Condition condition)
+    {
+        int totalTime = 0;
+        do
+        {
+            totalTime += sleepExponential(100);
+        }
+        while (!condition.validate());
+        return totalTime;
     }
 }
