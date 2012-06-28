8efbb944efa6dbfe92d294b8e661b30294f77792
Koks Nieger <koksnieger@gmail.com>
2012-06-28 17:42:42 +0100

    Paint a dungeon map overlay on the game screen.

--- src/org/half/HalfDungeon.java
+++ src/org/half/HalfDungeon.java
@@ -12,6 +12,7 @@ import org.powerbot.game.api.Manifest;
 import org.powerbot.game.bot.event.listener.PaintListener;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 @Manifest(
         name = "HalfDungeon",
@@ -28,15 +29,16 @@ public final class HalfDungeon extends ActiveScript implements PaintListener
         provide(new MousePaint());
 
         provide(new Lobby());
-        provide(new DungeonStart());
-
         provide(new RoomBuilder());
+
+        provide(new DungeonStart());
     }
 
     @Override
     public void onRepaint(Graphics graphics)
     {
         Graphics2D g = (Graphics2D) graphics;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         if (Dungeon.inDungeon())
         {
@@ -46,6 +48,47 @@ public final class HalfDungeon extends ActiveScript implements PaintListener
             {
                 currentRoom.draw(g);
             }
+        }
+
+        // draw overlay map
+        ArrayList<Room> rooms = Dungeon.getRooms();
+
+        // find top left coordinates
+        Point lowestCoordinates = new Point(0, 0);
+        for (Room room : rooms)
+        {
+            Point roomCoordinates = room.getCoordinates();
+            if (roomCoordinates.x < lowestCoordinates.x)
+            {
+                lowestCoordinates.x = roomCoordinates.x;
+            }
+            if (roomCoordinates.y < lowestCoordinates.y)
+            {
+                lowestCoordinates.y = roomCoordinates.y;
+            }
+        }
+
+        for (Room room : rooms)
+        {
+            Point roomCoordinates = room.getCoordinates();
+
+            // draw this room
+            if (roomCoordinates.x == 0 && roomCoordinates.y == 0)
+            {
+                g.setColor(new Color(0, 0, 255, 64));
+            }
+            else if (Avatar.findRoom() == room)
+            {
+                g.setColor(new Color(0, 128, 0, 128));
+            }
+            else
+            {
+                g.setColor(new Color(0, 128, 0, 64));
+            }
+            g.fillRect(240 + 34 * (roomCoordinates.x - lowestCoordinates.x), 10 + 34 * (roomCoordinates.y - lowestCoordinates.y), 30, 30);
+
+            g.setColor(new Color(0, 0, 0, 192));
+            g.drawRect(240 + 34 * (roomCoordinates.x - lowestCoordinates.x), 10 + 34 * (roomCoordinates.y - lowestCoordinates.y), 30, 30);
         }
     }
 }
