9104d4365b9c5cea99415f2e3acbc71b05c91007
Koks Nieger <koksnieger@gmail.com>
2012-06-28 17:40:30 +0100

    Improved room changing detection strategy.

--- src/org/half/dungeon/strategies/RoomBuilder.java
+++ src/org/half/dungeon/strategies/RoomBuilder.java
@@ -5,19 +5,30 @@ import org.half.dungeon.Dungeon;
 import org.half.dungeon.rooms.Room;
 import org.powerbot.concurrent.Task;
 import org.powerbot.concurrent.strategy.Strategy;
+import org.powerbot.game.api.methods.Game;
 
 public class RoomBuilder extends Strategy implements Task
 {
     @Override
     public boolean validate()
     {
-        return Dungeon.inDungeon() && Avatar.findRoom() == null;
+        Room currentRoom = Avatar.getCurrentRoom();
+        return Game.getClientState() != 12 && Dungeon.inDungeon() && (currentRoom == null || !currentRoom.contains(Avatar.getLocation()));
     }
 
     @Override
     public void run()
     {
-        Room room = new Room(Avatar.getLocation());
-        Dungeon.getRooms().add(room);
+        // Check if we already mapped this room
+        Room avatarRoom = Avatar.findRoom();
+        if (avatarRoom == null)
+        {
+            // Room isn't mapped, so map it
+            avatarRoom = new Room(Avatar.getLocation());
+            Dungeon.getRooms().add(avatarRoom);
+        }
+
+        // Change avatar current room
+        Avatar.setCurrentRoom(avatarRoom);
     }
 }
