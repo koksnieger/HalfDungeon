a426202e92ed2442bdf27ad9326d5f83397ce01a
Koks Nieger <koksnieger@gmail.com>
2012-06-30 18:18:43 +0100

    Initial class dor puzzle doors.

---
+++ src/org/half/dungeon/doors/PuzzleDoor.java
@@ -0,0 +1,33 @@
+package org.half.dungeon.doors;
+
+import org.half.dungeon.rooms.Room;
+import org.powerbot.game.api.wrappers.node.SceneObject;
+
+public class PuzzleDoor extends Door
+{
+    public static final int OBJECT_DOOR_PUZZLE_SLIDING_STATUES_FROZEN = 49375;
+    public static final int OBJECT_DOOR_PUZZLE_MONOLITH_FROZEN = 49462;
+    public static final int OBJECT_DOOR_PUZZLE_COLOURED_FERRETS_FROZEN = 54360;
+    public static final int[] OBJECT_DOOR_PUZZLE = {
+            OBJECT_DOOR_PUZZLE_SLIDING_STATUES_FROZEN,
+            OBJECT_DOOR_PUZZLE_MONOLITH_FROZEN,
+            OBJECT_DOOR_PUZZLE_COLOURED_FERRETS_FROZEN
+    };
+
+    /**
+     * Creates a puzzle door object.
+     *
+     * @param room     The room where this door is.
+     * @param position The position of this door in the room. (0 = North, 1 = East, 2 = South, 3 = West)
+     * @param object   The scene object associated with this door.
+     */
+    protected PuzzleDoor(final Room room, final int position, final SceneObject object)
+    {
+        super(room, position, object);
+    }
+
+    public String toString()
+    {
+        return getCompassPosition() + " Door: Puzzle";
+    }
+}
